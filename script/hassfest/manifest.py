d4497b6c9b8ab328aa27019074aedf6552be9474
Erik <erik@montnemery.com>
2022-08-15 14:47:30 +0200

    Add support for USB dongles to the hardware integration

--- script/hassfest/manifest.py
+++ script/hassfest/manifest.py
@@ -57,6 +57,7 @@ NO_IOT_CLASS = [
     "history",
     "homeassistant",
     "homeassistant_alerts",
+    "homeassistant_sky_connect",
     "homeassistant_yellow",
     "image",
     "input_boolean",
